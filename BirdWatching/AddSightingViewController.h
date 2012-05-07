2d82bf2ca7289993c17f30cc39d0dbb4b8e2836a
Joseph Caudle <joseph.caudle@gmail.com>
2012-05-06 19:18:17 -0500

    Added files for add scene and updated storyboard.

---
+++ BirdWatching/AddSightingViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddSightingViewController.h
+//  BirdWatching
+//
+//  Created by Joseph Caudle on 5/6/12.
+//  Copyright (c) 2012 The Atonement Academy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddSightingViewController : UITableViewController
+
+@end
